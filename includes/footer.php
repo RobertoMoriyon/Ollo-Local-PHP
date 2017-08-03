d37986b59de45bff6b8f0dc5a775276992450062
Roberto Moriyón <moriyon@gmail.com>
2017-08-03 13:43:51 +0200

    Several Updates

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <footer>
+        <p>&copy; Moriyon 2017</p>
+    </footer> 
+</div>
--- index.php
+++ index.php
@@ -7,51 +7,19 @@
 
 <body>
 
-    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-           <span class="navbar-toggler-icon"></span>
-         </button>
-        <a class="navbar-brand" href="#">Olloniego</a>
-
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="jugadores.html">Jugadores</a>
-                </li>
-                <li class="divider"></li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdown01">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-        </div>
-    </nav>
+    <?php include_once ("includes/nav.php");?>
 
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
         <div class="container">
             <h1 class="display-3">Hola!</h1>
-            <p>This is a template for a simple marketing or informational website. </p>
-            <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+            <p>Vienes al próximo partido?. </p>
+            <p><a class="btn btn-primary btn-lg" href="#" role="button">Convocatoria &raquo;</a></p>
         </div>
     </div>
 
     <div class="container">
-        <!-- Example row of columns -->
+        <!-- Example row of columns
         <div class="row">
             <div class="col-md-4">
                 <h2>Heading</h2>
@@ -70,26 +38,20 @@
                 <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
         </div>
-
+        
         <hr>
 
         <footer>
-            <p>&copy; Company 2017</p>
-        </footer>
+            <p>&copy; Moriyon 2017</p>
+        </footer>-->
     </div>
     <!-- /container -->
 
+        <?php include_once ("includes/footer.php");?>
 
+        <?php include_once ("includes/scripts.php");?>
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script>
-        window.jQuery || document.write('<script src="js/jquery-3.2.1.min.js"><\/script>')
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
+    
 </body>
 
 </html>
